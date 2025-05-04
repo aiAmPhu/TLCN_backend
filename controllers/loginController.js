2ee9120dfd8b08c7e08d3ea909a5a71b825f033d
Thiên Phú <126048504+aiAmPhu@users.noreply.github.com>
2025-05-04 19:15:51 +0700

    feat: add logout function

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,8 +2,10 @@ import User from "../models/user.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
+
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
+export const tokenBlacklist = new Set();
 
 export const loginFunction = async (req, res) => {
     const { email, password } = req.body;
@@ -35,5 +37,19 @@ export const loginFunction = async (req, res) => {
     } catch (error) {
         console.error("Lỗi đăng nhập:", error);
         return res.status(500).json({ message: "Lỗi hệ thống. Vui lòng thử lại sau." });
+    }
+};
+
+export const logoutFunction = (req, res) => {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({ message: "Không tìm thấy token" });
+    }
+    try {
+        jwt.verify(token, JWT_SECRET);
+        tokenBlacklist.add(token); // Thêm token vào blacklist
+        return res.status(200).json({ message: "Đăng xuất thành công" });
+    } catch (err) {
+        return res.status(401).json({ message: "Token không hợp lệ" });
     }
 };
--- routes/jwtRoutes.js
+++ routes/jwtRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { loginFunction } from "../controllers/loginController.js";
+import { loginFunction, logoutFunction } from "../controllers/loginController.js";
+import { authenticate } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/login", loginFunction);
+router.post("/logout", authenticate, logoutFunction);
 
 export default router;
