6489aad2154dc88e06820ea6ef47828500e0c583
Thiên Phú <126048504+aiAmPhu@users.noreply.github.com>
2025-05-12 00:51:39 +0700

    fix: Authenticate For GetAll Functions

--- routes/adbRoutes.js
+++ routes/adbRoutes.js
@@ -11,7 +11,7 @@ import { authenticate, authorizeRoles } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post("/add", authenticate, authorizeRoles("admin"), addAdBlock);
-router.get("/getall", authenticate, authorizeRoles("admin"), getAllAdBlocks);
+router.get("/getall", getAllAdBlocks);
 router.get("/getSubjects/:admissionBlockId", authenticate, authorizeRoles("admin"), getAllSubjectsByAdmissionBlockId);
 router.put("/update/:id", authenticate, authorizeRoles("admin"), updateAdBlock);
 router.delete("/delete/:id", authenticate, authorizeRoles("admin"), deleteAdBlock);
--- routes/adcRoutes.js
+++ routes/adcRoutes.js
@@ -5,7 +5,7 @@ import { authenticate, authorizeRoles } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post("/add", authenticate, authorizeRoles("admin"), addAdCriteria);
-router.get("/getall", authenticate, authorizeRoles("admin"), getAllAdCriterias);
+router.get("/getall", getAllAdCriterias);
 router.put("/update/:id", authenticate, authorizeRoles("admin"), updateAdCriteria);
 router.delete("/delete/:id", authenticate, authorizeRoles("admin"), deleteAdCriteria);
 export default router;
