d28f1ed34d08f74b88c00185bbe247d5a33fb594
Thiên Phú <126048504+aiAmPhu@users.noreply.github.com>
2025-05-25 15:16:24 +0700

    chore: remove unnecessary code

--- routes/adwRoutes.js
+++ routes/adwRoutes.js
@@ -1,13 +1,7 @@
 import express from "express";
 import {
     addAdmissionWish,
-    // getHighestPriorityAdmissionWishByUID,
     getAllWishesByUID,
-    //getAllUniqueEmails,
-    getWishesByStatus,
-    acceptWish,
-    rejectWish,
-    waitingtWish,
     getAcceptedWish,
     filterAdmissionResults,
     resetAllWishesStatus,
@@ -16,14 +10,8 @@ import { authenticate, authorizeRoles } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/add", authenticate, authorizeRoles("user", "admin", "admin"), addAdmissionWish);
-// router.get("/max/:uId", getHighestPriorityAdmissionWishByUID);
-router.get("/getAll/:uId", authenticate, authorizeRoles("user", "admin"), getAllWishesByUID);
-//router.get("/getUniqueEmails", getAllUniqueEmails);
-router.get("/getByStatus/:status", authenticate, authorizeRoles("admin", "admin"), getWishesByStatus);
-router.put("/accept/:id", authenticate, authorizeRoles("admin"), acceptWish);
-router.put("/reject/:id", authenticate, authorizeRoles("admin"), rejectWish);
-router.put("/waiting/:id", authenticate, authorizeRoles("admin"), waitingtWish);
+router.post("/add", authenticate, authorizeRoles("user", "admin"), addAdmissionWish);
+router.get("/getAll/:uId", authenticate, authorizeRoles("user"), getAllWishesByUID);
 router.get("/getAccepted", authenticate, authorizeRoles("admin"), getAcceptedWish);
 router.put("/filter", authenticate, authorizeRoles("admin"), filterAdmissionResults);
 router.put("/resetStatus", authenticate, authorizeRoles("admin"), resetAllWishesStatus);
